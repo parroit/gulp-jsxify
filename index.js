4971344844f9deeb5c88c5ca06eb83375589ec5d
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-06-13 19:20:52 +0200

    only require used tags to avoid circular dependency within modules

--- index.js
+++ index.js
@@ -2,13 +2,34 @@
 
 var gutil = require('gulp-util');
 var through = require('through2');
+var htmlTags = require('html-tags');
 
+function pseudoGuessTags(html) {
+    var tagsRe = /<\s*(\w+)/g;
+    var match = tagsRe.exec(html);
+    var results = {};
+    var tag;
+
+    while (match !== null) {
+        if (match) {
+            tag = match[1];
+            if ( htmlTags.indexOf( tag ) !== -1 ) {
+                tag = 'React';
+            }
+            
+            results[tag] = true;    
+        }
+        match = tagsRe.exec(html);
+        
+    }
+    return Object.keys(results);
+}
 
 module.exports = function(options) {
     options = options || {};
     options.requires = options.requires || [];
     options.requires.React = options.requires.React || 'react';
-    
+
     return through.obj(function(file, enc, cb) {
         if (file.isNull()) {
             this.push(file);
@@ -21,16 +42,24 @@ module.exports = function(options) {
         }
 
         var str = file.contents.toString();
+        var usedTags = pseudoGuessTags(str);
+        console.log(usedTags);
         var requires = Object.keys(options.requires)
             
+            .filter (function(tag){
+                return usedTags.indexOf(tag) > -1;
+            })
+
             .map(function(reqName) {
                 var reqPath = options.requires[reqName];
 
-                return 'var ' + reqName + ' = require(\'' + reqPath +'\');' ;
+                return 'var ' + reqName + ' = require(\'' + reqPath + '\');';
             })
 
+
+
             .join('\n');
-        
+
 
         var jsxContent = '/** @jsx React.DOM */\n' +
             '\'use strict\';\n' +
@@ -40,6 +69,8 @@ module.exports = function(options) {
             str +
             '    );\n' +
             '};';
+
+
 
         file.contents = new Buffer(jsxContent);
         file.path = gutil.replaceExtension(file.path, '.jsx');
